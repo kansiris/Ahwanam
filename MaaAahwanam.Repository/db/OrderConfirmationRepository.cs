da945056e8513a62db8a0101a384ea8d8137f5e8
kurarameshsai <kurarameshsai@gmail.com>
2016-08-16 18:35:16 +0530

    Updated code 06:32 PM

---
+++ MaaAahwanam.Repository/db/OrderConfirmationRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaaAahwanam.Models;
+
+namespace MaaAahwanam.Repository.db
+{
+    public class OrderConfirmationRepository
+    {
+        MaaAahwanamEntities maaAahwanamEntities = new MaaAahwanamEntities();
+        public List<orderconfirmation_Result> GetOrderConfirmation()
+        {
+            var list= maaAahwanamEntities.orderconfirmation().ToList();
+            return list;
+        }
+    }
+}
