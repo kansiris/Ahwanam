4cf93dea7cb63ab4f3aae88efd336c03370d922f
sai krishna reddy <saikrishnareddythummala@gmail.com>
2016-08-16 18:28:00 +0530

    Changes

---
+++ MaaAahwanam.Web/Controllers/OrderConfirmationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using MaaAahwanam.Repository;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class OrderConfirmationController : Controller
+    {
+        // GET: OrderConfirmation
+        public ActionResult Index()
+        {
+            OrderConfirmationService orderConfirmationService = new OrderConfirmationService();
+            List<orderconfirmation_Result> list= orderConfirmationService.GetOrderConfirmation();
+            ViewBag.Orderconfirmation = list;
+            return View();
+        }
+    }
+}
