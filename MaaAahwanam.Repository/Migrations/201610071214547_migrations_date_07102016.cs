c75eb917520a15d8d694db4f48b70b96f8bd8d1b
kurarameshsai <kurarameshsai@gmail.com>
2016-10-07 18:00:31 +0530

    staged

--- MaaAahwanam.Repository/Migrations/201610071214547_migrations_date_07102016.cs
+++ MaaAahwanam.Repository/Migrations/201610071214547_migrations_date_07102016.cs
@@ -3,18 +3,18 @@ namespace MaaAahwanam.Repository.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class migrations_date_100720162 : DbMigration
+    public partial class migrations_date_07102016 : DbMigration
     {
         public override void Up()
         {
+            AddColumn("dbo.EventInformations", "subid", c => c.Long(nullable: false));
             AddColumn("dbo.OrderDetails", "subid", c => c.Long(nullable: false));
-            DropColumn("dbo.UserDetails", "subid");
         }
         
         public override void Down()
         {
-            AddColumn("dbo.UserDetails", "subid", c => c.Long(nullable: false));
             DropColumn("dbo.OrderDetails", "subid");
+            DropColumn("dbo.EventInformations", "subid");
         }
     }
 }
