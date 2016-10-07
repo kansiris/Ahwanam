29241d4e7a42202bd780941afae77e52d1956ac3
xsisaikrishna <saikrishna@xsilica.com>
2016-10-07 17:29:37 +0530

    Staged

--- MaaAahwanam.Repository/Migrations/201610070731157_migrations_date_100720162.Designer.cs
+++ MaaAahwanam.Repository/Migrations/201610070731157_migrations_date_100720162.Designer.cs
@@ -7,13 +7,13 @@ namespace MaaAahwanam.Repository.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class migrations_date_10062016 : IMigrationMetadata
+    public sealed partial class migrations_date_100720162 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(migrations_date_10062016));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(migrations_date_100720162));
         
         string IMigrationMetadata.Id
         {
-            get { return "201610061307394_migrations_date_10062016"; }
+            get { return "201610070731157_migrations_date_100720162"; }
         }
         
         string IMigrationMetadata.Source
