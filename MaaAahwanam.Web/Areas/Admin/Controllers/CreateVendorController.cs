1d869f6116d39c26eb9b6b2f240cfac14b198cbb
kurarameshsai <rameshsai@xsilica.com>
2016-11-10 18:25:53 +0530

    latest

--- MaaAahwanam.Web/Areas/Admin/Controllers/CreateVendorController.cs
+++ MaaAahwanam.Web/Areas/Admin/Controllers/CreateVendorController.cs
@@ -2079,7 +2079,7 @@ namespace MaaAahwanam.Web.Areas.Admin.Controllers
             }
             if (Command == "Add Deal")
             {
-                deal.VendorType = "TravelAccomodation";
+                deal.VendorType = "Travel&Accommodation";
                 deal.VendorId = long.Parse(id);
                 deal.VendorSubId = long.Parse(vid);
                 deal = dealService.AddDealService(deal);
