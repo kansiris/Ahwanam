7b0dd562ac5644a84c09513b1a7eadd3c1622c0e
kurarameshsai <rameshsai@xsilica.com>
2017-12-18 20:09:23 +0530

    latest

--- MaaAahwanam.Web/Controllers/VendorParticularServiceController.cs
+++ MaaAahwanam.Web/Controllers/VendorParticularServiceController.cs
@@ -16,6 +16,8 @@ namespace MaaAahwanam.Web.Controllers
         // GET: VendorParticularService
         public ActionResult Index(string type, string id, string vid)
         {
+            if (type == "Conventions" || type == "Resorts" || type == "Hotels")
+                type = "Venue";
             ViewBag.Productinfo = productInfoService.getProductsInfo_Result(int.Parse(id), type, int.Parse(vid)); //GetProductsInfo_Result Productinfo
             ViewBag.vendor = null;
             if (type == "Venue")
@@ -28,6 +30,10 @@ namespace MaaAahwanam.Web.Controllers
                 ViewBag.Photography = venorVenueSignUpService.GetParticularVendorPhotography(long.Parse(id), long.Parse(vid));
             else if (type == "Other")
                 ViewBag.Other = venorVenueSignUpService.GetParticularVendorOther(long.Parse(id), long.Parse(vid));
+            int imagescount = (ViewBag.Productinfo.image != null) ? ViewBag.Productinfo.image.Split(",").Count() : 0;
+            ViewBag.image1 = (imagescount > 0) ? ViewBag.Productinfo.image.Split(",")[0] : null;
+            ViewBag.image2 = (imagescount > 1) ? ViewBag.Productinfo.image.Split(",")[1] : null;
+            ViewBag.image3 = (imagescount > 2) ? ViewBag.Productinfo.image.Split(",")[2] : null;
             return View();
         }
     }
