3b9758cbccfe9a44674b6c06353f2633769e3ba8
kurarameshsai <rameshsai@xsilica.com>
2018-01-16 21:43:40 +0530

    latest

--- MaaAahwanam.Web/Controllers/HomePageController.cs
+++ MaaAahwanam.Web/Controllers/HomePageController.cs
@@ -61,6 +61,7 @@ namespace MaaAahwanam.Web.Controllers
                 EmailSendingUtility emailSendingUtility = new EmailSendingUtility();
                 emailSendingUtility.Email_maaaahwanam("seema@xsilica.com", "Quotation Sent to Vendor", "Mail From Ahwanam");
                 emailSendingUtility.Email_maaaahwanam("amit.saxena@ahwanam.com", "Quotation Sent to Vendor", "Mail From Ahwanam");
+                emailSendingUtility.Email_maaaahwanam("krameshsai1@gmail.com", "Quotation Sent to Vendor", "Mail From Ahwanam");
                 if (quotation > 0)
                     return Json("Success");
                 else
