9fe455590f63774fa934ae00a4c061eb033d0d66
kurarameshsai <rameshsai@xsilica.com>
2018-02-16 12:58:06 +0530

    latest

--- MaaAahwanam.Web/Controllers/VendorProductsController.cs
+++ MaaAahwanam.Web/Controllers/VendorProductsController.cs
@@ -25,5 +25,29 @@ namespace MaaAahwanam.Web.Controllers
                 ViewBag.records = vendorProductsService.Getvendorproducts_Result(service);
             return View();
         }
+
+        public ActionResult SearchResult(string location, string category, string subcategory, string startdate, string enddate, string minguest, string maxguest, string minbudget, string maxbudget)
+        {
+            string[] splittedcategories = category.Split(',');
+            string[] splittedsubcategories = subcategory.Split(',');
+            var selectedvenuescategories= ""; var selectedvenues = "";
+            selectedvenuescategories = String.Join(",", Enumerable.Range(0, splittedcategories.Length).Where(i => splittedcategories[i] == "Venues").ToList());
+            for (int i = 0; i < selectedvenuescategories.Split(',').Length; i++)
+            {
+                selectedvenues = selectedvenues +','+splittedsubcategories[int.Parse(selectedvenuescategories.Split(',')[i])].ToString();
+            }
+            //for (int i = 0; i < subcategory.Split(',').Length; i++)
+            //{
+            //    ViewBag.Venue = vendorProductsService.Getvendorproducts_Result("Venue").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();
+            //    ViewBag.Hotels = vendorProductsService.Getvendorproducts_Result("Hotel").Where(m => m.businessname == subcategory.Split(',')[i]).ToList(); // Hotel records
+            //    ViewBag.Resorts = vendorProductsService.Getvendorproducts_Result("Resort").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();// Resort records
+            //    ViewBag.Conventions = vendorProductsService.Getvendorproducts_Result("Convention Hall").Where(m => m.businessname == subcategory.Split(',')[i]).ToList(); // Convention records
+            //    ViewBag.Catering = vendorProductsService.Getvendorproducts_Result("Catering").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();
+            //    ViewBag.Photography = vendorProductsService.Getvendorproducts_Result("Photography").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();
+            //    ViewBag.Decorator = vendorProductsService.Getvendorproducts_Result("Decorator").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();
+            //    ViewBag.Mehendi = vendorProductsService.Getvendorproducts_Result("Mehendi").Where(m => m.businessname == subcategory.Split(',')[i]).ToList();
+            //}
+            return View("SearchResult");
+        }
     }
 }
