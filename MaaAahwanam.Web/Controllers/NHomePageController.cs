42bc10e5d78f7528f16e1f154551a02d8a3e6c89
kurarameshsai <rameshsai@xsilica.com>
2018-03-13 12:00:58 +0530

    latest

---
+++ MaaAahwanam.Web/Controllers/NHomePageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class NHomePageController : Controller
+    {
+        // GET: NHomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
