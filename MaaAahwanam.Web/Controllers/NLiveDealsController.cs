7fdb1c9017869f94b2735fec8473cfd0827ab182
kurarameshsai <rameshsai@xsilica.com>
2018-03-15 19:28:26 +0530

    latest

---
+++ MaaAahwanam.Web/Controllers/NLiveDealsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class NLiveDealsController : Controller
+    {
+        // GET: NLiveDeals
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
