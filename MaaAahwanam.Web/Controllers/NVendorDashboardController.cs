cb603925b366ad36c17c97dde71c8020dd3581d4
kurarameshsai <rameshsai@xsilica.com>
2018-04-27 19:33:38 +0530

    latest

--- MaaAahwanam.Web/Controllers/NVendorDashboardController.cs
+++ MaaAahwanam.Web/Controllers/NVendorDashboardController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MaaAahwanam.Service;
+using MaaAahwanam.Models;
 
 namespace MaaAahwanam.Web.Controllers
 {
     public class NVendorDashboardController : Controller
     {
+        VendorMasterService vendorMasterService = new VendorMasterService();
         // GET: NVendorDashboard
         public ActionResult Index(string id)
         {
             ViewBag.id = id;
+            ViewBag.Vendor = vendorMasterService.GetVendor(long.Parse(id));
             return View();
         }
     }
