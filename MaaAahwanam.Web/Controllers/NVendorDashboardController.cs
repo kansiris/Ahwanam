cf1f8fb15bbbae2b0c3c16409f7d55ea5be46e48
kurarameshsai <rameshsai@xsilica.com>
2018-04-30 15:38:38 +0530

    latest

--- MaaAahwanam.Web/Controllers/NVendorDashboardController.cs
+++ MaaAahwanam.Web/Controllers/NVendorDashboardController.cs
@@ -5,17 +5,26 @@ using System.Web;
 using System.Web.Mvc;
 using MaaAahwanam.Service;
 using MaaAahwanam.Models;
+using MaaAahwanam.Web.Custom;
+using MaaAahwanam.Service;
 
 namespace MaaAahwanam.Web.Controllers
 {
     public class NVendorDashboardController : Controller
     {
+        OrderService orderService = new OrderService();
         VendorMasterService vendorMasterService = new VendorMasterService();
         // GET: NVendorDashboard
         public ActionResult Index(string id)
         {
-            ViewBag.id = id;
-            ViewBag.Vendor = vendorMasterService.GetVendor(long.Parse(id));
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+                ViewBag.id = id;
+                ViewBag.Vendor = vendorMasterService.GetVendor(long.Parse(id));
+                var orders = orderService.userOrderList().Where(m => m.UserLoginId == (int)user.UserId);
+                ViewBag.order = orders.OrderByDescending(m => m.OrderId);
+            }
             return View();
         }
     }
