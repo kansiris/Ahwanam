a6fbba52d01d303893e9c90b58d9913480d9ea4e
xsisireesh <xsisireesh@SIREESH>
2018-06-21 18:25:07 +0530

    www.kansiris.org

--- MaaAahwanam.Web/Areas/Admin/Controllers/ContestsManagementController.cs
+++ MaaAahwanam.Web/Areas/Admin/Controllers/ContestsManagementController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using MaaAahwanam.Service;
 using MaaAahwanam.Utility;
 using System.IO;
+using System.Net.Mail;
 
 namespace MaaAahwanam.Web.Areas.Admin.Controllers
 {
@@ -129,16 +130,12 @@ namespace MaaAahwanam.Web.Areas.Admin.Controllers
 
                 emailSendingUtility.Email_maaaahwanam(emailid, txtmessage, subj);
 
-                var email1 = "prabodh.dasari@xsilica.com";
-                var txtmes = "Contestant is activated";
-                readFile = readFile.Replace("[Message]", txtmes);
-                readFile = readFile.Replace("[ActivationLink]", url);
-                readFile = readFile.Replace("[name]", username);
-                string txtmessage1 = readFile;//readFile + body;
+               
+                string txtmessage1 = username +" "+ "is activated for father's day contest";
 
                 string subj1 = "Contestant is activated";
 
-                emailSendingUtility.Email_maaaahwanam(email1, txtmessage1, subj1);
+                emailSendingUtility.Email_maaaahwanam("prabodh.dasari@xsilica.com", txtmessage1, subj1);
 
 
                 contestsService.Activationcontest(cont1,command );
