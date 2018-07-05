e7cce5a33fb9ff8bce0491d6f8da4b5938bfc6e5
kurarameshsai <rameshsai@xsilica.com>
2018-07-05 19:14:46 +0530

    latest

--- MaaAahwanam.Web/Controllers/NVendorsController.cs
+++ MaaAahwanam.Web/Controllers/NVendorsController.cs
@@ -1,9 +1,12 @@
-﻿using MaaAahwanam.Service;
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using MaaAahwanam.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MaaAahwanam.Web.Custom;
 
 namespace MaaAahwanam.Web.Controllers
 {
@@ -11,6 +14,8 @@ namespace MaaAahwanam.Web.Controllers
     {
         // GET: NVendors
         VendorProductsService vendorProductsService = new VendorProductsService();
+        QuotationListsService quotationListsService = new QuotationListsService();
+
         public ActionResult Index(string servicetype)
         {
             ViewBag.service = servicetype;
@@ -25,8 +30,40 @@ namespace MaaAahwanam.Web.Controllers
             else if (servicetype == "FunctionHall")
             { ViewBag.records = vendorProductsService.Getvendorproducts_Result("Function Hall"); ViewBag.service = "Function Hall"; }
             else
-                ViewBag.records = vendorProductsService.Getvendorproducts_Result(servicetype);
+            { ViewBag.records = vendorProductsService.Getvendorproducts_Result(servicetype); }
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetQuote(QuotationsList quotationsList)
+        {
+            string ip = HttpContext.Request.UserHostAddress;
+            //string hostName = Dns.GetHostName();
+            quotationsList.IPaddress = ip;//Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+            quotationsList.UpdatedTime = DateTime.UtcNow;
+            quotationsList.Status = "Active";
+            int count = quotationListsService.GetVendorVenue(quotationsList.IPaddress).Count;
+
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                count = 0;
+            }
+
+            if (count < 6)
+            {
+                int quotation = quotationListsService.AddQuotationList(quotationsList);
+                EmailSendingUtility emailSendingUtility = new EmailSendingUtility();
+                string msg = "Email ID:"+quotationsList.EmailId+",Name:"+quotationsList.Name+",Phone:"+quotationsList.PhoneNo+",Check-IN Date:"+quotationsList.EventStartDate+",Check-IN Time:"+quotationsList.EventStartTime+",Check-OUT Date:"+quotationsList.EventEnddate+",Check-OUT Time:"+quotationsList.EventEndtime+",Persons:"+quotationsList.Persons+",Extra Persons:"+quotationsList.ExtraPersons+"";
+                emailSendingUtility.Email_maaaahwanam("seema@xsilica.com", msg, "Mail From Ahwanam");
+                emailSendingUtility.Email_maaaahwanam("amit.saxena@ahwanam.com", msg, "Mail From Ahwanam");
+                emailSendingUtility.Email_maaaahwanam("rameshsai@xsilica.com", msg, "Mail From Ahwanam");
+                if (quotation > 0)
+                    return Json("Success");
+                else
+                    return Json("Fail");
+            }
+            else
+                return Json("exceeded");
         }
     }
 }
--- MaaAahwanam.Web/Scripts/Quote.js
+++ MaaAahwanam.Web/Scripts/Quote.js
@@ -17,3 +17,23 @@
         }
     })
 }
+
+function GetNewQuote(quotationsList) {
+    $.ajax({
+        url: '/NVendors/GetQuote',
+        type: "POST",
+        datatype: "json",
+        data: { quotationsList: quotationsList },
+        success: function (response) {
+            if (response == "Success")
+            { alert("Quotation Sent to Vendor"); $(".view-details-alert").css("display", "none"); }
+            else if (response == "exceeded")
+                alert("Quotation Limit Exceeded!!! please Login/Sign up to Raise Quotation");
+            else if (response == "Fail")
+                alert("Cannot Raise Quotation at the moment")
+        },
+        error: function (er) {
+            errormsg("Something Went Wrong!!!Try Again Later");
+        }
+    })
+}
