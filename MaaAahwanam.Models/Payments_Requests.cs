37ff96b8922653428274c6d781e10e736f62e80c
kurarameshsai <rameshsai@xsilica.com>
2018-07-05 12:29:35 +0530

    latest

--- MaaAahwanam.Models/Payments_Requests.cs
+++ MaaAahwanam.Models/Payments_Requests.cs
@@ -16,5 +16,22 @@ namespace MaaAahwanam.Models
         public string cardnumber { get; set; }
         public string CVV { get; set; }
         public DateTime Paiddate { get; set; }
+        public string Amount { get; set; }
+        public string Currency { get; set; }
+        public string International { get; set; }
+        public string Payment_Method { get; set; }
+        public string Refunded_Amount { get; set; }
+        public string Refund_Status { get; set; }
+        public string Payment_Captured { get; set; }
+        public string Card_ID { get; set; }
+        public string Bank { get; set; }
+        public string Wallet { get; set; }
+        public string Customer_Email { get; set; }
+        public string Customer_Contact { get; set; }
+        public string Fee { get; set; }
+        public string Tax { get; set; }
+        public string Error_Code { get; set; }
+        public string Error_Description { get; set; }
+        public string Payment_Status { get; set; }
     }
 }
