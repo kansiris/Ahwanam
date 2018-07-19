be6d0181c1302392cc7093cd48a77e852cb5436a
kurarameshsai <rameshsai@xsilica.com>
2018-07-19 19:41:44 +0530

    latest

--- MaaAahwanam.Web/Areas/Admin/Controllers/QuotationsController.cs
+++ MaaAahwanam.Web/Areas/Admin/Controllers/QuotationsController.cs
@@ -4,17 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MaaAahwanam.Service;
+using MaaAahwanam.Repository;
 
 namespace MaaAahwanam.Web.Areas.Admin.Controllers
 {
     public class QuotationsController : Controller
     {
+        ProductInfoService productInfoService = new ProductInfoService();
         QuotationListsService quotationListsService = new QuotationListsService();
         VendorDatesService vendorDatesService = new VendorDatesService();
         // GET: Admin/Quotations
         public ActionResult Index()
         {
-            ViewBag.quotations = quotationListsService.GetAllQuotations().Where(m=>m.Status == "Active").ToList();
+            ViewBag.quotations = quotationListsService.GetAllQuotations().Where(m => m.Status == "Active").ToList();
             return View();
         }
 
@@ -25,16 +27,59 @@ namespace MaaAahwanam.Web.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public ActionResult FilteredVendors(string type,string date)
+        public ActionResult FilteredVendors(string type, string date)
         {
-            DateTime convertdate = Convert.ToDateTime(date);
-            //var selecteddate = new DateTime(convertdate.Year, convertdate.Month, convertdate.Day);
-            ViewBag.vendors = vendorDatesService.GetVendorsByService().Where(m => m.ServiceType == type && m.StartDate >= convertdate || m.EndDate <= convertdate).ToList();
-            if (ViewBag.vendors.Count == 0)
+            if (type != null && date != null)
             {
-                ViewBag.novendor = "No Vendors Available On This Date";
+                var data = vendorDatesService.GetVendorsByService().Where(m => m.ServiceType == type).ToList();
+                ViewBag.display = "1";
+                ViewBag.records = seperatedates(data, date, type);
+            }
+            else if (type == null && date == null)
+            {
+                ViewBag.novendor = "Select Service Type & Date To View Vendors";//"No Vendors Available On This Date";
             }
             return PartialView("FilteredVendors", "Quotations");
         }
+
+        public List<string[]> seperatedates(List<filtervendordates_Result> data, string date, string type)
+        {
+            List<string[]> betweendates = new List<string[]>();
+            string dates = "";
+            //var Gettotaldates = vendorDatesService.GetDates(long.Parse(id), long.Parse(vid));
+            int recordcount = data.Count();
+            foreach (var item in data)
+            {
+                var startdate = Convert.ToDateTime(item.StartDate);
+                var enddate = Convert.ToDateTime(item.EndDate);
+                if (startdate != enddate)
+                {
+                    string orderdates = productInfoService.disabledate(item.masterid, item.subid, type);
+                    for (var dt = startdate; dt <= enddate; dt = dt.AddDays(1))
+                    {
+                        dates = (dates != "") ? dates + "," + dt.ToString("dd-MM-yyyy") : dt.ToString("dd-MM-yyyy");
+                    }
+                    if (dates.Split(',').Contains(date))
+                    {
+                        if (orderdates != "")
+                            dates = String.Join(",", dates.Split(',').Where(i => !orderdates.Split(',').Any(e => i.Contains(e))));
+                        string[] da = { item.masterid.ToString(), item.subid.ToString(), dates, item.BusinessName, item.ServiceType, item.VenueType, item.Type };
+                        betweendates.Add(da);
+                    }
+                    dates = "";
+                }
+                else
+                {
+                    if (dates.Contains(date))
+                    {
+                        string[] da = { item.masterid.ToString(), item.subid.ToString(), startdate.ToString("dd-MM-yyyy"), item.BusinessName, item.ServiceType, item.VenueType, item.Type };
+                        betweendates.Add(da);
+                    }
+                    dates = "";
+                }
+            }
+            return betweendates;
+        }
+
     }
 }
