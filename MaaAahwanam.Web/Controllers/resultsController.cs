c75989c19e03a63597a7cf23998831517334b257
kurarameshsai <rameshsai@xsilica.com>
2018-08-12 09:37:24 +0530

    latest

--- MaaAahwanam.Web/Controllers/resultsController.cs
+++ MaaAahwanam.Web/Controllers/resultsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MaaAahwanam.Service;
+using MaaAahwanam.Repository;
 
 namespace MaaAahwanam.Web.Controllers
 {
@@ -14,17 +15,27 @@ namespace MaaAahwanam.Web.Controllers
         public ActionResult Index(string type)
         {
             type = (type == null) ? "Venue" : type;
-            ViewBag.venues = resultsPageService.GetAllVendors(type);//.Take(6).ToList();
+            ViewBag.venues = resultsPageService.GetAllVendors(type).Take(6).ToList();
+            ViewBag.count = 6;
             return View();
         }
 
-        //public PartialViewResult Loadmore(string count,string type)
+        //public PartialViewResult Loadmore(string count, string type)
         //{
         //    type = (type == null) ? "Venue" : type;
-        //    int takecount = (count == "" || count == null) ? 6 : int.Parse(count) +6;
+        //    int takecount = (count == "" || count == null) ? 6 : int.Parse(count) + 6;
         //    ViewBag.count = takecount;
         //    ViewBag.venues = resultsPageService.GetAllVendors(type).Take(takecount).ToList();
         //    return PartialView("Loadmore");
         //}
+
+        public JsonResult Loadmore(string count, string type)
+        {
+            type = (type == null) ? "Venue" : type;
+            int takecount = (count == "" || count == null) ? 6 : int.Parse(count) * 6;
+            ViewBag.count = takecount;
+            List<GetVendors_Result> vendorslist = resultsPageService.GetAllVendors(type).Skip(takecount).Take(6).ToList();
+            return Json(vendorslist);
+        }
     }
 }
