ed49d2032b22e6c75b226afca7ff2a19d7170672
kurarameshsai <rameshsai@xsilica.com>
2018-08-28 18:16:53 +0530

    latest

--- MaaAahwanam.Web/Controllers/viewserviceController.cs
+++ MaaAahwanam.Web/Controllers/viewserviceController.cs
@@ -11,6 +11,7 @@ using MaaAahwanam.Utility;
 using System.Net;
 using System.IO;
 using MaaAahwanam.Repository;
+using System.Text;
 
 namespace MaaAahwanam.Web.Controllers
 {
@@ -25,7 +26,7 @@ namespace MaaAahwanam.Web.Controllers
         //private static TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
         ResultsPageService resultsPageService = new ResultsPageService();
 
-        
+
         // GET: viewservice
         public ActionResult Index(string name, string type, string id)
         {
@@ -41,7 +42,7 @@ namespace MaaAahwanam.Web.Controllers
             var venues = viewservicesss.GetVendorVenue(long.Parse(id)).ToList();
             if (type == "Venue" || type == "Convention" || type == "Banquet" || type == "Function")
             {
-                var data = viewservicesss.GetVendor(long.Parse(id)); 
+                var data = viewservicesss.GetVendor(long.Parse(id));
                 var allimages = viewservicesss.GetVendorAllImages(long.Parse(id)).ToList();
                 ViewBag.image = (allimages.Count() != 0) ? allimages.FirstOrDefault().ImageName.Replace(" ", "") : null;
                 ViewBag.allimages = allimages;
@@ -57,6 +58,7 @@ namespace MaaAahwanam.Web.Controllers
                 List<VendorVenue> vendor = venues;
                 List<SPGETNpkg_Result> package = new List<SPGETNpkg_Result>();
                 List<VendorVenue> amenities = new List<VendorVenue>();
+                List<string> famenities = new List<string>();
                 foreach (var item in venues)
                 {
                     package.AddRange(viewservicesss.getvendorpkgs(id).Where(p => p.VendorSubId == long.Parse(item.Id.ToString())).ToList());
@@ -64,8 +66,9 @@ namespace MaaAahwanam.Web.Controllers
                 }
                 ViewBag.particularVenue = vendor;
                 ViewBag.availablepackages = package;
-                ViewBag.amenities = amenities.Select(m => new
+                var allamenities = amenities.Select(m => new
                 {
+                    #region Venue amenities
                     m.AC,
                     m.TV,
                     m.ComplimentaryBreakfast,
@@ -108,66 +111,24 @@ namespace MaaAahwanam.Web.Controllers
                     m.Intercom,
                     m.SufficientRoomSize,
                     m.SufficientWashroom
-                }).ToList(); 
+                    #endregion
+                }).ToList();
+                
+                foreach (var item in allamenities)
+                {
+                    string value = string.Join(",",item).Replace("{","").Replace("}","");
+                    var availableamenities = value.Split(',');
+                    value = "";
+                    for (int i = 0; i < availableamenities.Length; i++)
+                    {
+                        if (availableamenities[i].Split('=')[1].Trim() == "Yes")
+                            value = value + "," + availableamenities[i].Split('=')[0].Trim();
+                    }
+                    famenities.Add(value.TrimStart(','));
+                }
+                ViewBag.amenities = famenities;
             }
             return View();
-        }
-
-        public void Amenities(List<VendorVenue> vendor)
-        {
-            List<VendorVenue> final = new List<VendorVenue>();
-            foreach (var item in vendor)
-            {
-                VendorVenue loadrecords = item; //new List<VendorVenue>();
-                final.Add(item);
-                //var availableamenities = loadrecords.Select(m => new
-                //{
-                //    m.AC,
-                //    m.TV,
-                //    m.ComplimentaryBreakfast,
-                //    m.Geyser,
-                //    m.ParkingFacility,
-                //    m.CardPayment,
-                //    m.LiftorElevator,
-                //    m.BanquetHall,
-                //    m.Laundry,
-                //    m.CCTVCameras,
-                //    m.SwimmingPool,
-                //    m.ConferenceRoom,
-                //    m.Bar,
-                //    m.DiningArea,
-                //    m.PowerBackup,
-                //    m.WheelchairAccessible,
-                //    m.RoomHeater,
-                //    m.InRoomSafe,
-                //    m.MiniFridge,
-                //    m.InhouseRestaurant,
-                //    m.Gym,
-                //    m.HairDryer,
-                //    m.PetFriendly,
-                //    m.HDTV,
-                //    m.Spa,
-                //    m.WellnessCenter,
-                //    m.Electricity,
-                //    m.BathTub,
-                //    m.Kitchen,
-                //    m.Netflix,
-                //    m.Kindle,
-                //    m.CoffeeTeaMaker,
-                //    m.SofaSet,
-                //    m.Jacuzzi,
-                //    m.FullLengthMirrror,
-                //    m.Balcony,
-                //    m.KingBed,
-                //    m.QueenBed,
-                //    m.SingleBed,
-                //    m.Intercom,
-                //    m.SufficientRoomSize,
-                //    m.SufficientWashroom
-                //});
-                //final.AddRange(loadrecords);
-            }
-            
         }
     }
 }
