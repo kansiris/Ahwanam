45c45daffb0a6eff2a301c1ac9e4abe29a0e0943
xsisireesh <xsisireesh@SIREESH>
2018-08-29 13:41:43 +0530

    ks

---
+++ MaaAahwanam.Repository/db/cartrepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaaAahwanam.Models;
+using MaaAahwanam.Repository;
+
+namespace MaaAahwanam.Repository.db
+{
+   public class cartrepository
+    {
+        readonly ApiContext _dbContext = new ApiContext();
+        MaaAahwanamEntities maaAahwanamEntities = new MaaAahwanamEntities();
+        public List<SPGETNpkg_Result> getvendorpkgs(string id)
+        {
+            return maaAahwanamEntities.SPGETNpkg(Convert.ToInt64(id)).ToList();
+        }
+    }
+}
