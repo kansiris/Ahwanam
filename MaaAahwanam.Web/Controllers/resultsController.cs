0b117d62013108d8b4fdd786ea40e5d9984ec823
xsisireesh <xsisireesh@SIREESH>
2018-09-10 14:41:51 +0530

    ks

--- MaaAahwanam.Web/Controllers/resultsController.cs
+++ MaaAahwanam.Web/Controllers/resultsController.cs
@@ -5,22 +5,62 @@ using System.Web;
 using System.Web.Mvc;
 using MaaAahwanam.Service;
 using MaaAahwanam.Repository;
+using MaaAahwanam.Web.Custom;
+using System.IO;
+using MaaAahwanam.Utility;
+using MaaAahwanam.Models;
 
 namespace MaaAahwanam.Web.Controllers
 {
     public class resultsController : Controller
     {
+        UserLoginDetailsService userLoginDetailsService = new UserLoginDetailsService();
+        Vendormaster vendorMaster = new Vendormaster();
         ResultsPageService resultsPageService = new ResultsPageService();
+       // Homeco
         // GET: results
         public ActionResult Index(string type, string loc, string eventtype, string count, string date)
         {
+           // string url =  HttpContext.Current.Request.Url.AbsoluteUri;
+
             type = (type == null) ? "Venue" : type;
             var data = resultsPageService.GetAllVendors(type);
             ViewBag.venues = data.Take(6).ToList();
             ViewBag.minprice = data.Select(m => m.cost1).Min();
             ViewBag.maxprice = data.Select(m => m.cost1).Max();
             ViewBag.count = 6;
-            return View();
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+
+                if (user.UserType == "User")
+                {
+                    string Email = user.Username;
+
+                    string txtto = "amit.saxena@ahwanam.com,rameshsai@xsilica.com,sireesh.k@xsilica.com";
+                    int id = Convert.ToInt32(user.UserId);
+                    var userdetails = userLoginDetailsService.GetUser(id);
+                    string ipaddress = HttpContext.Request.UserHostAddress;
+
+                    string username = userdetails.FirstName;
+                    HomeController home = new HomeController();
+                    username = home.Capitalise(username);
+                    string emailid = user.Username;
+                    FileInfo File = new FileInfo(Server.MapPath("/mailtemplate/login.html"));
+                    string readFile = File.OpenText().ReadToEnd();
+                  //  readFile = readFile.Replace("[ActivationLink]", url);
+                    readFile = readFile.Replace("[name]", username);
+                    readFile = readFile.Replace("[Ipaddress]", ipaddress);
+                    readFile = readFile.Replace("[email]", Email);
+
+                    string txtmessage = readFile;//readFile + body;
+                    string subj = "User login from ahwanam";
+                    EmailSendingUtility emailSendingUtility = new EmailSendingUtility();
+                    emailSendingUtility.Email_maaaahwanam(txtto, txtmessage, subj);
+                }
+            }
+
+                    return View();
         }
 
         public PartialViewResult Loadmore(string count, string type)
