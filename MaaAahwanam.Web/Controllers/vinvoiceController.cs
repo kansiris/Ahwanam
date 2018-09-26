146aa3f3241a468af6fc96ee616b6bb9413381a4
kurarameshsai <rameshsai@xsilica.com>
2018-09-26 20:01:52 +0530

    latest

--- MaaAahwanam.Web/Controllers/vinvoiceController.cs
+++ MaaAahwanam.Web/Controllers/vinvoiceController.cs
@@ -13,8 +13,10 @@ namespace MaaAahwanam.Web.Controllers
     {
         VendorMasterService vendorMasterService = new VendorMasterService();
         UserLoginDetailsService userLoginDetailsService = new UserLoginDetailsService();
+        OrderService orderService = new OrderService();
+        OrderdetailsServices orderdetailService = new OrderdetailsServices();
         // GET: vinvoice
-        public ActionResult Index()
+        public ActionResult Index(string oid)
         {
             if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
             {
@@ -23,6 +25,18 @@ namespace MaaAahwanam.Web.Controllers
                 string email = userLoginDetailsService.Getusername(long.Parse(id));
                 Vendormaster Vendormaster = vendorMasterService.GetVendorByEmail(email);
                 ViewBag.Vendor = vendorMasterService.GetVendor(Convert.ToInt64(Vendormaster.Id));
+                if (oid != null && oid != "")
+                {
+                    var orderdetails = orderService.userOrderList().Where(m=>m.OrderId == long.Parse(oid)).ToList();
+                    ViewBag.orderid = oid;
+                    ViewBag.username = orderdetails.FirstOrDefault().FirstName + " " + orderdetails.FirstOrDefault().LastName;
+                    ViewBag.vendorname = orderdetails.FirstOrDefault().BusinessName;
+                    ViewBag.vendoraddress = orderdetails.FirstOrDefault().Address +","+ orderdetails.FirstOrDefault().Landmark +","+orderdetails.FirstOrDefault().City;
+                    ViewBag.vendorcontact = orderdetails.FirstOrDefault().ContactNumber;
+                    ViewBag.orderdate = Convert.ToDateTime(orderdetails.FirstOrDefault().BookedDate).ToString("MMM d,yyyy");
+                    ViewBag.orderdetails = orderdetails;
+                    ViewBag.totalprice = orderdetails.FirstOrDefault().TotalPrice;
+                }
             }
             return View();
         }
