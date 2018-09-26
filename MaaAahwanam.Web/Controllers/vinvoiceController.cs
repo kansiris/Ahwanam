149067b1ccd8b0e8bca85ff24efb4beeab1a3ca5
kurarameshsai <rameshsai@xsilica.com>
2018-09-26 18:37:56 +0530

    latest

---
+++ MaaAahwanam.Web/Controllers/vinvoiceController.cs
@@ -0,0 +1,30 @@
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using MaaAahwanam.Web.Custom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaaAahwanam.Web.Controllers
+{
+    public class vinvoiceController : Controller
+    {
+        VendorMasterService vendorMasterService = new VendorMasterService();
+        UserLoginDetailsService userLoginDetailsService = new UserLoginDetailsService();
+        // GET: vinvoice
+        public ActionResult Index()
+        {
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+                string id = user.UserId.ToString();
+                string email = userLoginDetailsService.Getusername(long.Parse(id));
+                Vendormaster Vendormaster = vendorMasterService.GetVendorByEmail(email);
+                ViewBag.Vendor = vendorMasterService.GetVendor(Convert.ToInt64(Vendormaster.Id));
+            }
+            return View();
+        }
+    }
+}
---
+++ MaaAahwanam.Web/newdesignstyles/Scripts/landingpage.js
@@ -0,0 +1,46 @@
+﻿function contactform(val) {
+    var fname = $('#fname').val();
+    var lname = $('#lname').val();
+    var emailid = $('#emailid').val();
+    var phoneno = $('#phoneno').val();
+    var eventdate = $('#datetimepicker').val();
+    if (fname == '') {
+        $('#fname').focus();
+        alert("Please Enter Your First Name");
+    }
+    else if (fname == '') {
+        $('#fname').focus();
+        alert("Please Enter Your First Name");
+    }
+    else if (lname == '') {
+        $('#lname').focus();
+        alert("Please Enter Your Last Name");
+    }
+    else if (emailid == '') {
+        $('#emailid').focus();
+        alert("Please Enter Your Email ID");
+    }
+    else if (phoneno == '') {
+        $('#phoneno').focus();
+        alert("Please Enter Your Phone Number");
+    }
+    else if (eventdate == '') {
+        $('#datetimepicker').focus();
+        alert("Please Select Your Event Date");
+    }
+    else {
+        var url = '/' + val + '/SendEmail?fname=' + fname + '&&lname=' + lname + '&&emailid=' + emailid + '&&phoneno=' + phoneno + '&&eventdate=' + eventdate;
+        $.ajax({
+            url: url,
+            method: 'POST',
+            contentType: 'application-json',
+            success: function (response) {
+                if (response == 'success') {
+                    $('#contactusdiv').css('display', 'none');
+                    $('#successmsg').css('display', 'block');
+                    gtag_report_conversion();
+                }
+            }
+        });
+    }
+}
