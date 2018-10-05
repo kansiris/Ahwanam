73bf819348d2fb7df2b15c62321037bb16dcea6c
xsisireesh <xsisireesh@SIREESH>
2018-10-05 19:29:30 +0530

    kscdd

--- MaaAahwanam.Web/Controllers/vinvoiceController.cs
+++ MaaAahwanam.Web/Controllers/vinvoiceController.cs
@@ -114,43 +114,44 @@ namespace MaaAahwanam.Web.Controllers
         {
             HomeController home = new HomeController();
           
-            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
-                string vid = user.UserId.ToString();
-                ViewBag.userid = vid;
-                string email = userLoginDetailsService.Getusername(long.Parse(vid));
-                ViewBag.vemail = email;
-                Vendormaster Vendormaster = vendorMasterService.GetVendorByEmail(email);
-                List<Payment> payment = rcvpaymentservice.getPayments(oid);
-                var userlogdetails = mnguserservice.getuserbyid(Convert.ToInt32(vid));
-                string txtto = userlogdetails.email;
+            //if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            //{
+            //    var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+            //    string vid = user.UserId.ToString();
+            //    ViewBag.userid = vid;
+            //    string email = userLoginDetailsService.Getusername(long.Parse(vid));
+            //    ViewBag.vemail = email;
+            //    Vendormaster Vendormaster = vendorMasterService.GetVendorByEmail(email);
+            //    var orderdetails1 = orderService.userOrderList1().Where(m => m.OrderId == long.Parse(oid)).ToList();
+            //    List<Payment> payment = rcvpaymentservice.getPayments(oid);
+            //    var userlogdetails = mnguserservice.getuserbyid(Convert.ToInt32((orderdetails1.FirstOrDefault()).UpdatedBy));
+            //    string txtto = userlogdetails.email;
 
-                string name = userlogdetails.firstname;
-                name = home.Capitalise(name);
-                //string OrderId = Convert.ToString(order.OrderId);
-                StringBuilder cds = new StringBuilder();
-                cds.Append("<table style='border:1px black solid;'><tbody>");
-                cds.Append("<tr><td> Payment Id</td><td> Payment Type </td><td> Payment Date </td><td> Received Amount </td></tr>");
-                foreach (var item in payment)
-                {
-                    cds.Append("<tr><td style = 'width: 75px;border: 1px black solid;'> " + item.Payment_Id + "</td><td style = 'width: 75px;border: 1px black solid;' > " + item.Payment_Type + " </td><td style = 'width: 75px;border: 1px black solid;'> " + item.Payment_Date + " </td><td style = 'width: 50px;border: 1px black solid;'> " + item.Received_Amount + " </td></tr>");  //<td style = 'width: 50px;border: 2px black solid;'> " + item.eventstartdate + " </td><td> date </td>
-                }
-                cds.Append("</tbody></table>");
-                //string url = Request.Url.Scheme + "://" + Request.Url.Authority;
-                string url = cds.ToString();
-                FileInfo File = new FileInfo(Server.MapPath("/mailtemplate/order.html"));
-                string readFile = File.OpenText().ReadToEnd();
-                readFile = readFile.Replace("[ActivationLink]", url);
-                readFile = readFile.Replace("[name]", name);
-                //readFile = readFile.Replace("[orderid]", OrderId);
-                string txtmessage = readFile;//readFile + body;
-                string subj = "Thanks for your order";
-                EmailSendingUtility emailSendingUtility = new EmailSendingUtility();
-                emailSendingUtility.Email_maaaahwanam(txtto, txtmessage, subj);
-                emailSendingUtility.Email_maaaahwanam("seema@xsilica.com ", txtmessage, subj);
+            //    string name = userlogdetails.firstname;
+            //    name = home.Capitalise(name);
+            //    //string OrderId = Convert.ToString(order.OrderId);
+            //    StringBuilder cds = new StringBuilder();
+            //    cds.Append("<table style='border:1px black solid;'><tbody>");
+            //    cds.Append("<tr><td> Payment Id</td><td> Payment Type </td><td> Payment Date </td><td> Received Amount </td></tr>");
+            //    foreach (var item in payment)
+            //    {
+            //        cds.Append("<tr><td style = 'width: 75px;border: 1px black solid;'> " + item.Payment_Id + "</td><td style = 'width: 75px;border: 1px black solid;' > " + item.Payment_Type + " </td><td style = 'width: 75px;border: 1px black solid;'> " + item.Payment_Date + " </td><td style = 'width: 50px;border: 1px black solid;'> " + item.Received_Amount + " </td></tr>");  //<td style = 'width: 50px;border: 2px black solid;'> " + item.eventstartdate + " </td><td> date </td>
+            //    }
+            //    cds.Append("</tbody></table>");
+            //    //string url = Request.Url.Scheme + "://" + Request.Url.Authority;
+            //    string url = cds.ToString();
+            //    FileInfo File = new FileInfo(Server.MapPath("/mailtemplate/order.html"));
+            //    string readFile = File.OpenText().ReadToEnd();
+            //    readFile = readFile.Replace("[ActivationLink]", url);
+            //    readFile = readFile.Replace("[name]", name);
+            //    //readFile = readFile.Replace("[orderid]", OrderId);
+            //    string txtmessage = readFile;//readFile + body;
+            //    string subj = "Thanks for your order";
+            //    EmailSendingUtility emailSendingUtility = new EmailSendingUtility();
+            //    emailSendingUtility.Email_maaaahwanam(txtto, txtmessage, subj);
+            //    emailSendingUtility.Email_maaaahwanam("seema@xsilica.com ", txtmessage, subj);
                 
-            }
+            //}
 
             return Json("success", JsonRequestBehavior.AllowGet);
 
