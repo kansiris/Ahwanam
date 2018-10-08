79ed4d7154dfbe683f0227cbc9dcf7173dce7c13
kurarameshsai <rameshsai@xsilica.com>
2018-10-08 13:04:49 +0530

    latest

--- MaaAahwanam.Web/newdesignstyles/Scripts/landingpage.js
+++ MaaAahwanam.Web/newdesignstyles/Scripts/landingpage.js
@@ -4,7 +4,7 @@
     var emailid = $('#emailid').val();
     var phoneno = $('#phoneno').val();
     var eventdate = $('#datetimepicker').val();
-    var mob = /^[1-9]{1}[0-9]{9}$/;
+    //var mob = /^[1-9]{1}[0-9]{9}$/;
     var eml = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if (fname == '') {
         $('#fname').focus();
@@ -31,11 +31,11 @@
         $('#phoneno').focus();
         alert("Please Enter Your Phone Number");
     }
-    else if (mob.test(phoneno) == false) {
-        $("#phoneno").focus();
-        alert("Please Enter Valid Mobile Number.");
-        return false;
-    }
+    //else if (mob.test(phoneno) == false) {
+    //    $("#phoneno").focus();
+    //    alert("Please Enter Valid Mobile Number.");
+    //    return false;
+    //}
     else if (eventdate == '') {
         $('#datetimepicker').focus();
         alert("Please Select Your Event Date");
@@ -56,3 +56,46 @@
         });
     }
 }
+
+var input = document.querySelector("#phoneno"), errorMsg = document.querySelector("#error-msg");
+var iti = window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: function (success, failure) {
+
+        $.get("https://ipinfo.io", function () { }, "jsonp").always(function (resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            success(countryCode);
+        });
+    },
+    utilsScript: "newdesignstyles/Scripts/utils.js",
+});
+
+var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+input.addEventListener('blur', function () {
+    reset();
+    if (input.value.trim()) {
+        if (iti.isValidNumber()) {
+            //validMsg.classList.remove("hide");
+            $('#btnsubmit').removeAttr('disabled');
+        } else {
+            input.classList.add("error");
+            var errorCode = iti.getValidationError();
+            errorMsg.innerHTML = errorMap[errorCode];
+            errorMsg.classList.remove("hide");
+            if (errorMap[errorCode] == 'Invalid number' || errorMap[errorCode] == 'Invalid country code') {
+                $("#phoneno").val('').focus();
+            }
+            else {
+                $("#phoneno").focus();
+            }
+            $('#btnsubmit').attr('disabled', 'disabled');
+        }
+    }
+});
+
+var reset = function () {
+    input.classList.remove("error");
+    errorMsg.innerHTML = "";
+    errorMsg.classList.add("hide");
+};
