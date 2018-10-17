bc240b39a2564fd01853c4cb61bab7df1e5e01ee
lakshmixsilica <lakshmi.p@xsilica.com>
2018-10-17 20:23:32 +0530

    Reseller

---
+++ MaaAahwanam.Repository/db/PartnerRepository.cs
@@ -0,0 +1,21 @@
+﻿using MaaAahwanam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaaAahwanam.Repository.db
+{
+   public class PartnerRepository
+    {
+        readonly ApiContext _dbContext = new ApiContext();
+
+        public Partner AddPartner(Partner partner)
+        {
+            _dbContext.Partner.Add(partner);
+            _dbContext.SaveChanges();
+            return partner;
+        }
+    }
+}
--- MaaAahwanam.Web/Controllers/ReSellerManagementController.cs
+++ MaaAahwanam.Web/Controllers/ReSellerManagementController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using MaaAahwanam.Web.Custom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,35 @@ namespace MaaAahwanam.Web.Controllers
 {
     public class ReSellerManagementController : Controller
     {
+        VendorVenueService vendorVenueService = new VendorVenueService();
+        viewservicesservice viewservicesss = new viewservicesservice();
+        newmanageuser newmanageuse = new newmanageuser();
+        Vendormaster vendorMaster = new Vendormaster();
+        private static TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+        PartnerService partnerservice = new PartnerService();
         // GET: ReSellerManagement
         public ActionResult Index()
         {
-            return View();
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+                string uid = user.UserId.ToString();
+                string vemail = newmanageuse.Getusername(long.Parse(uid));
+                vendorMaster = newmanageuse.GetVendorByEmail(vemail);
+               string VendorId = vendorMaster.Id.ToString();
+                ViewBag.masterid = VendorId;
+
+               ViewBag.package = viewservicesss.getvendorpkgs(VendorId).ToList();
+            }
+                return View();
+        }
+        [HttpPost]
+        public JsonResult Index(Partner partner)
+        {
+            partner.RegisteredDate = DateTime.Now;
+            partner.UpdatedDate = DateTime.Now;
+            partner = partnerservice.AddPartner(partner);
+            return Json("Sucess", JsonRequestBehavior.AllowGet);
         }
     }
 }
