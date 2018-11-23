d08e9c9a85ff57afb7a1c0f0c1d55d6204f0be54
lakshmixsilica <lakshmi.p@xsilica.com>
2018-11-23 15:43:08 +0530

    latest

--- MaaAahwanam.Web/Controllers/duepaymentController.cs
+++ MaaAahwanam.Web/Controllers/duepaymentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MaaAahwanam.Models;
+using MaaAahwanam.Service;
+using MaaAahwanam.Web.Custom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,26 @@ namespace MaaAahwanam.Web.Controllers
 {
     public class duepaymentController : Controller
     {
+        VendorVenueService vendorVenueService = new VendorVenueService();
+        newmanageuser newmanageuse = new newmanageuser();
+        Vendormaster vendorMaster = new Vendormaster();
+        VendorMasterService vendorMasterService = new VendorMasterService();
+        PartnerService partnerservice = new PartnerService();
+        private static TimeZoneInfo INDIAN_ZONE = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
         // GET: duepayment
         public ActionResult Index()
         {
-            return View();
+            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                var user = (CustomPrincipal)System.Web.HttpContext.Current.User;
+                string uid = user.UserId.ToString();
+                string vemail = newmanageuse.Getusername(long.Parse(uid));
+                vendorMaster = newmanageuse.GetVendorByEmail(vemail);
+                 string VendorId = vendorMaster.Id.ToString();
+                ViewBag.masterid = VendorId;
+                ViewBag.Vendor = vendorMasterService.GetVendor(Convert.ToInt64(VendorId));
+                }
+                return View();
         }
     }
 }
