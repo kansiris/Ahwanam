354b77ce2ca819445325643d8a9c863e200b6719
lakshmixsilica <lakshmi.p@xsilica.com>
2019-02-28 20:33:11 +0530

    latest

---
+++ MaaAahwanam.Repository/db/CeremonyRepository.cs
@@ -0,0 +1,25 @@
+﻿using MaaAahwanam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace MaaAahwanam.Repository.db
+{
+   public class CeremonyRepository
+    {
+        readonly ApiContext _dbContext = new ApiContext();
+        public List<Ceremony> Getall()
+        {
+            return _dbContext.Ceremony.ToList();
+        }
+
+        public CeremonyCategory getceremonycategory(long id)
+        {
+            return _dbContext.CeremonyCategory.Where(c => c.CeremonyId == id).FirstOrDefault();
+        }
+    }
+}
