4bcd2aaa590a81df0c5352fd18eb28475039ac96
kurarameshsai <rameshsai@xsilica.com>
2019-04-10 19:02:07 +0530

    latest

--- MaaAahwanam.Service/WhishListService.cs
+++ MaaAahwanam.Service/WhishListService.cs
@@ -16,22 +16,22 @@ namespace MaaAahwanam.Service
 
         DateTime updateddate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, INDIAN_ZONE);
 
-        public AvailableWhishLists AddWhishList(AvailableWhishLists availableWhishLists)
-        {
-            availableWhishLists.Status = "InActive";
-            availableWhishLists.WhishListedDate = updateddate;
-            return whishListRepository.AddWhishList(availableWhishLists);
-        }
+        //public AvailableWhishLists AddWhishList(AvailableWhishLists availableWhishLists)
+        //{
+        //    availableWhishLists.Status = "InActive";
+        //    availableWhishLists.WhishListedDate = updateddate;
+        //    return whishListRepository.AddWhishList(availableWhishLists);
+        //}
 
-        public List<AvailableWhishLists> GetWhishList(string id)
-        {
-            return whishListRepository.GetUserWhishlist(id);
-        }
+        //public List<AvailableWhishLists> GetWhishList(string id)
+        //{
+        //    return whishListRepository.GetUserWhishlist(id);
+        //}
 
-        public string RemoveWhishList(int WhishListID)
-        {
-            return whishListRepository.RemoveWhishList(WhishListID);
-        }
+        //public string RemoveWhishList(int WhishListID)
+        //{
+        //    return whishListRepository.RemoveWhishList(WhishListID);
+        //}
 
         public wishlist Addwishlist(wishlist wishlists)
         {
